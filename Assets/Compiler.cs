0b4c9774a1fb133e01f2884dccdaacdcce452a1e
Dünya Kılavuz <dunyakilavuz@gmail.com>
2015-11-15 23:58:07 +0200

    Started scripting

---
+++ Assets/Compiler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Compiler : MonoBehaviour
+{
+
+	public static void Compile(string code)
+	{
+		char[] codeArray;
+		codeArray = code.ToCharArray();
+
+
+	}
+
+
+}
---
+++ Assets/VehicleControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleControl : MonoBehaviour 
+{
+	float movementSpeed = 2f;
+	float rotationSpeed = 0.5f;
+	bool easyMode = true;
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+		if (easyMode == true)
+		{
+			EasyControl ();
+		} 
+		else 
+		{
+			HardControl ();
+		}
+	}
+
+	void EasyControl()
+	{
+		if (Input.GetKey (KeyCode.W) == true)
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+		}
+		if (Input.GetKey (KeyCode.S) == true)
+		{
+			transform.Translate (Vector3.back * Time.deltaTime * movementSpeed);
+		}
+		if (Input.GetKey (KeyCode.A) == true)
+		{
+			transform.Rotate(0,-rotationSpeed,0);
+		}
+		if (Input.GetKey (KeyCode.D) == true)
+		{
+			transform.Rotate(0,rotationSpeed,0);
+		}
+	}
+
+	void HardControl()
+	{
+		Compiler.Compile ("Hello");
+	}
+
+}
